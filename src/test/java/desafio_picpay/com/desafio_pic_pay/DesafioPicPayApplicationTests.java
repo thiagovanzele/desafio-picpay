4fde285db77bacaf32752e849822d3a19c260f31
Thiago Vanzele <thiagovanzele@gmail.com>
2026-02-08 17:21:57 -0300

    first commit

---
+++ src/test/java/desafio_picpay/com/desafio_pic_pay/DesafioPicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio_picpay.com.desafio_pic_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
